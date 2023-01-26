97f0e9fbee5f4af5e2be6fe98f32425ae1a465bc
enzolivelli2000 <enzo.livelli@gmail.com>
2023-01-26 17:57:40 +0000

    [ADD] basic model from github

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,31 +1,31 @@
-# -*- coding: utf-.8 -*-
+# -*- coding: utf-8 -*-
 
 {
     'name': 'Odoo Academy',
     
     'summary': """Academy app to manage Training""",
-    
-    'description':"""
-        Academy MOdule to manage Training:
+
+    'description': """
+        Academy Module to manage Training:
         - Courses
         - Sessions
-        - Attendees"""
-    ,
+        - Attendees
+        """,
     
-    'author':'Odoo',
+    'author': 'Odoo',
     
     'website': 'https://www.odoo.com',
     
     'category': 'Training',
-    
     'version': '0.1',
     
     'depends': ['base'],
     
     'data': [
     ],
-    
     'demo': [
+        'demo/academy_demo.xml',
     ],
-    
+    #Add license to remove License Warning
+    'license': 'OPL-1'
 }
